6ff66a44121cfb4eb0b9f16f4e95ddb9b93104fe
bamblebam <burhanizrangwala@gmail.com>
2024-03-16 02:55:11 +0530

    lighthouse handlers

---
+++ src/handlers/lighthouse.handler.ts
@@ -0,0 +1,33 @@
+import lighthouse from "@lighthouse-web3/sdk";
+import fs from "fs";
+import axios from "axios";
+
+const { LIGHTHOUSE_API_KEY } = process.env;
+
+export const uploadFileToLighthouse = async (file_path: string) => {
+  const uploadResponse = await lighthouse.upload(
+    file_path,
+    LIGHTHOUSE_API_KEY as string
+  );
+  return uploadResponse;
+};
+
+export const retrieveFileFromLighthouse = async (
+  cid: string,
+  download_path: string
+) => {
+  axios
+    .get(`https://gateway.lighthouse.storage/ipfs/${cid}`, {
+      responseType: "arraybuffer",
+    })
+    .then((response) => {
+      const buffer = Buffer.from(response.data, "binary");
+      fs.writeFile(download_path, buffer, (err) => {
+        if (err) throw err;
+        console.log(`File saved to ${download_path}`);
+      });
+    })
+    .catch((error) => {
+      console.error("Failed to save the file:", error);
+    });
+};
