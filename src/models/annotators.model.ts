69439edb042d06d0c44826703a067e2695710d09
bamblebam <burhanizrangwala@gmail.com>
2024-03-16 06:33:06 +0530

    chore: world con

--- src/models/annotators.model.ts
+++ src/models/annotators.model.ts
@@ -3,6 +3,7 @@ import { IData } from "./data.model";
 
 export interface IAnnotator extends Document {
   //worldcoin stuff - TO ADD
+  nullifier_hash: string;
   wallet_address: string;
   data_assigned: IData["_id"][];
   createdAt: Date;
@@ -11,6 +12,7 @@ export interface IAnnotator extends Document {
 
 export const AnnotatorSchema: Schema = new Schema({
   //worldcoin stuff - TO ADD
+  nullifier_hash: { type: String, required: true },
   wallet_address: { type: String, required: true },
   data_assigned: [{ type: Schema.Types.ObjectId, ref: "data" }],
   createdAt: { type: Date, default: Date.now },
